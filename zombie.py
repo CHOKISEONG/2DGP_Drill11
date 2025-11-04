6cd64ec15b0624bf0765d494b1df79524b439ef9
Cho Kisung <kisung128@gmail.com>
2025-11-04 23:51:13 +0900

    좀비랑 볼 맞을 때 함수 호출 구현

--- zombie.py
+++ zombie.py
@@ -33,6 +33,7 @@ class Zombie:
         self.load_images()
         self.frame = random.randint(0, 9)
         self.dir = random.choice([-1,1])
+        self.lifeCnt = 2
 
 
     def get_bb(self):
@@ -51,11 +52,20 @@ class Zombie:
 
     def draw(self):
         if self.dir < 0:
-            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].composite_draw(0, 'h', self.x, self.y, self.lifeCnt * 100, self.lifeCnt * 100)
         else:
-            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, 200, 200)
+            Zombie.images['Walk'][int(self.frame)].draw(self.x, self.y, self.lifeCnt * 100, self.lifeCnt * 100)
         draw_rectangle(*self.get_bb())
 
     def handle_event(self, event):
         pass
+
+    def handle_collision(self, group, other):
+        if group == 'fire_ball:zombie':
+            print('fire_ball 맞음')
+            self.lifeCnt -= 1
+
+            if self.lifeCnt <= 0:
+                game_world.remove_object(self)
+
 
